e95063bc950a8c02c5c052fc5d49a0bef85653b4
wjszxli <wjszxli@163.com>
2018-09-25 18:03:55 +0800

    sql

--- libaisi_crm.sql
+++ libaisi_crm.sql
@@ -11,11 +11,34 @@
  Target Server Version : 50710
  File Encoding         : 65001
 
- Date: 05/09/2018 01:50:11
+ Date: 25/09/2018 17:55:38
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for cAuthor
+-- ----------------------------
+DROP TABLE IF EXISTS `cAuthor`;
+CREATE TABLE `cAuthor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `remark` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `user` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of cAuthor
+-- ----------------------------
+BEGIN;
+INSERT INTO `cAuthor` VALUES (1, 'finance', '财务权限', '18,19');
+INSERT INTO `cAuthor` VALUES (2, 'medium', '媒介权限', NULL);
+INSERT INTO `cAuthor` VALUES (3, 'sell', '销售权限', NULL);
+INSERT INTO `cAuthor` VALUES (4, 'charge', '主管权限', NULL);
+INSERT INTO `cAuthor` VALUES (5, 'master', '创始人权限', '18');
+COMMIT;
 
 -- ----------------------------
 -- Table structure for cCustomer
@@ -34,6 +57,7 @@ CREATE TABLE `cCustomer` (
   `isDelete` int(11) DEFAULT '0',
   `remark` varchar(1000) DEFAULT NULL,
   `customer` varchar(45) DEFAULT NULL,
+  `isDeptAdmin` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
 
@@ -41,14 +65,34 @@ CREATE TABLE `cCustomer` (
 -- Records of cCustomer
 -- ----------------------------
 BEGIN;
-INSERT INTO `cCustomer` VALUES (9, '公司名称', '客户品牌', '联系人', '电话', '微信', 'qq', 1, '跟进人', 0, '33333\n33322\n233\n2323\n2332\n2322323', NULL);
-INSERT INTO `cCustomer` VALUES (10, '杭州京东', '京东', '何家豪', '18870420552', '', '', 0, '', 0, '3333', NULL);
-INSERT INTO `cCustomer` VALUES (11, '娃哈哈', '水果', '联系我', '1', '1', '1', 1, '1', 0, NULL, NULL);
-INSERT INTO `cCustomer` VALUES (12, '测试合作', '品牌', '你好', '333', '33', '333', 0, '333', 0, NULL, NULL);
-INSERT INTO `cCustomer` VALUES (13, '公司名称', '2', '2222', '2222', '233', '23', 0, '23', 0, NULL, NULL);
-INSERT INTO `cCustomer` VALUES (14, '4444', '444', '444', '444', '444', '4444', 0, '444', 1, NULL, NULL);
-INSERT INTO `cCustomer` VALUES (15, '公司名称1', '客户品牌', '联系人', '电话', '微信', 'qq', 1, '跟进人', 0, NULL, NULL);
-INSERT INTO `cCustomer` VALUES (16, '公司名称1', '客户品牌2', '联系人', '电话', '微信', 'qq', 1, '跟进人', 0, NULL, NULL);
+INSERT INTO `cCustomer` VALUES (9, '公司名称', '客户品牌', '联系人', '电话', '微信', 'qq', 1, '跟进人', 0, '33333\n33322\n233\n2323\n2332\n2322323', NULL, 0);
+INSERT INTO `cCustomer` VALUES (10, '杭州京东', '京东', '何家豪', '18870420552', '', '', 0, '', 0, '3333', NULL, 0);
+INSERT INTO `cCustomer` VALUES (11, '娃哈哈', '水果', '联系我', '1', '1', '1', 1, '1', 0, NULL, NULL, 0);
+INSERT INTO `cCustomer` VALUES (12, '测试合作', '品牌', '你好', '333', '33', '333', 0, '333', 0, NULL, NULL, 0);
+INSERT INTO `cCustomer` VALUES (13, '公司名称', '2', '2222', '2222', '233', '23', 0, '23', 0, NULL, NULL, 0);
+INSERT INTO `cCustomer` VALUES (14, '4444', '444', '444', '444', '444', '4444', 0, '444', 1, NULL, NULL, 0);
+INSERT INTO `cCustomer` VALUES (15, '公司名称1', '客户品牌', '联系人', '电话', '微信', 'qq', 1, '跟进人', 0, NULL, NULL, 0);
+INSERT INTO `cCustomer` VALUES (16, '公司名称1', '客户品牌2', '联系人', '电话', '微信', 'qq', 1, '跟进人', 0, NULL, NULL, 0);
+COMMIT;
+
+-- ----------------------------
+-- Table structure for cDept
+-- ----------------------------
+DROP TABLE IF EXISTS `cDept`;
+CREATE TABLE `cDept` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deptName` varchar(45) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `deptId` varchar(45) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `remark` varchar(45) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `master` varchar(45) CHARACTER SET utf8mb4 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of cDept
+-- ----------------------------
+BEGIN;
+INSERT INTO `cDept` VALUES (1, '研发部', 'produce', '研发部门', NULL);
 COMMIT;
 
 -- ----------------------------
@@ -77,13 +121,13 @@ CREATE TABLE `cPlan` (
   `customerName` varchar(100) DEFAULT NULL,
   `financeReamrk` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of cPlan
 -- ----------------------------
 BEGIN;
-INSERT INTO `cPlan` VALUES (1, '333', 'top', '1534689459', '33', '33', 1, '3', 0, '6', '3333', NULL, NULL, '2018-08-20 00:06:46', 0, 0, 0, NULL, NULL, '3333\n这也是财务备注\n这还是财务备注');
+INSERT INTO `cPlan` VALUES (1, '333', 'top', '1534689459', '33', '33', 1, '3', 0, '6', '3333', NULL, NULL, '2018-08-20 00:06:46', 0, 0, 0, NULL, NULL, '3333\n这也是财务备注\n这还是财务备注\n财务备注');
 INSERT INTO `cPlan` VALUES (2, 'rrr', 'second', '1534690608', '23', '2', 1, '3', 0, '6', '2222', '32', NULL, '2018-08-20 00:06:46', 1, 1, 0, NULL, NULL, NULL);
 INSERT INTO `cPlan` VALUES (3, '公众号', 'second', '1534693436', '23', '12', 1, '3', 0, '6', '输入', '34', '管理员', '2018-08-20 00:06:46', 1, 1, 1, NULL, NULL, NULL);
 INSERT INTO `cPlan` VALUES (4, '测试的', 'second', '1534695181', '100', '2', 1, '3', 0, '6', '', '32', '管理员', '2018-08-20 00:13:20', 0, 0, 0, NULL, NULL, NULL);
@@ -109,6 +153,18 @@ INSERT INTO `cPlan` VALUES (23, '测试公众号', 'top', '1536076576', '3', '2'
 INSERT INTO `cPlan` VALUES (24, '测试公众号', 'second', '1537459064', '3', '3', 1, '3', 0, '6', '3', '0', '管理员', '2018-09-03 23:59:07', 0, 0, 0, '12', NULL, NULL);
 INSERT INTO `cPlan` VALUES (25, '测试公众号', 'second', '1535991057', '3', '2', 0, '6', 1, '3', '111', '0', '管理员', '2018-09-04 00:11:13', 0, 0, 0, '11', '娃哈哈', NULL);
 INSERT INTO `cPlan` VALUES (26, '测试公众号', 'top', '1536072270', '34', '2', 0, '3', 0, '6', '这是备注', '0', '管理员', '2018-09-04 22:44:43', 0, 0, 0, '10', '杭州京东', NULL);
+INSERT INTO `cPlan` VALUES (27, '测试公众号的数据啊', 'top', '1536327177', '3', '3', 1, '3', 0, '6', '4444', '2', '管理员', '2018-09-07 21:33:06', 0, 0, 0, '9', '公司名称', NULL);
+INSERT INTO `cPlan` VALUES (28, '胖少女晚托班', 'top', '1536327335', '34', '3', 1, '3', 0, '6', '333', '2', '管理员', '2018-09-07 21:35:44', 0, 0, 0, '9', '公司名称', NULL);
+INSERT INTO `cPlan` VALUES (29, '小思成长日记', 'second', '1536327443', '3', '2', 1, '3', 0, '6', '333', '3', '管理员', '2018-09-07 21:37:29', 0, 0, 0, '9', '公司名称', NULL);
+INSERT INTO `cPlan` VALUES (30, '小思成长日记', 'second', '1536327503', '3', '34', 1, '6', 0, '3', '333', '3', '管理员', '2018-09-07 21:38:32', 0, 0, 0, '9', '公司名称', NULL);
+INSERT INTO `cPlan` VALUES (31, '小思成长日记', 'top', '1536327575', '23', '2', 0, '3', 1, '6', '2222', '3', '管理员', '2018-09-07 21:39:44', 0, 0, 0, '9', '公司名称', NULL);
+INSERT INTO `cPlan` VALUES (32, '小思成长日记', 'top', '1536327618', '3', '3', 1, '3', 0, '6', '333', '3', '管理员', '2018-09-07 21:40:24', 0, 0, 0, '9', '公司名称', NULL);
+INSERT INTO `cPlan` VALUES (33, '小思成长日记', 'top', '1536327665', '2', '2', 1, '6', 0, '3', '222', '3', '管理员', '2018-09-07 21:41:18', 0, 0, 0, '10', '杭州京东', NULL);
+INSERT INTO `cPlan` VALUES (34, '测试公众号', 'top', '1538055727', '23', '2', 1, '3', 0, '6', '2222', '1', '管理员', '2018-09-07 21:42:20', 0, 0, 0, '9', '公司名称', NULL);
+INSERT INTO `cPlan` VALUES (35, '测试公众号', 'top', '1536327835', '2', '2', 0, '3', 1, '6', '', '1', '管理员', '2018-09-07 21:44:06', 0, 0, 0, '9', '公司名称', NULL);
+INSERT INTO `cPlan` VALUES (36, '测试公众号', 'top', '1536327944', '2', '3', 1, '3', 0, '6', '222', '1', '管理员', '2018-09-07 21:45:56', 0, 0, 0, '9', '公司名称', NULL);
+INSERT INTO `cPlan` VALUES (37, '小思成长日记', 'top', '1536328061', '3', '3', 1, '3', 0, '6', '333', '35', '管理员', '2018-09-07 21:47:53', 0, 0, 0, '9', '公司名称', NULL);
+INSERT INTO `cPlan` VALUES (38, '小思成长日记', 'other', '1536328080', '33', '3', 1, '3', 0, '6', '3333', '35', '管理员', '2018-09-07 21:48:10', 0, 0, 0, '10', '杭州京东', NULL);
 COMMIT;
 
 -- ----------------------------
@@ -142,16 +198,16 @@ CREATE TABLE `cPublicNumber` (
 -- ----------------------------
 BEGIN;
 INSERT INTO `cPublicNumber` VALUES (32, '测试公众号', '3', 1, '头条刊例', '头条成本', '次条刊例更新', '次条成本', '末条刊例更新', '末条成本', '1', '2018-09-05 01:24:25', 2, '上传', '教育亲子', NULL, '2222\n这是投放详情', '/public/upload/1536081865163_my.png');
-INSERT INTO `cPublicNumber` VALUES (33, '测试公众号', '3', 4, '头条刊例', '头条成本', '次条刊例更新', '次条成本', '末条刊例更新', '末条成本', '3', '2018-08-20 14:38:48', 0, '上传', '初高中', NULL, NULL, NULL);
+INSERT INTO `cPublicNumber` VALUES (33, '测试公众号', '3', 4, '头条刊例', '头条成本', '次条刊例更新', '次条成本', '末条刊例更新', '末条成本', '3', '2018-09-07 23:01:41', 0, '上传', '初高中', NULL, NULL, '/public/upload/1536330816232_favicon.png,/public/upload/1536330871704_favicon.png,/public/upload/1536332287213_favicon.png,/public/upload/1536332501647_favicon.png');
 INSERT INTO `cPublicNumber` VALUES (34, '测试公众号的数据啊', '3', 7, '头条刊例', '头条成本', '次条刊例更新', '次条成本', '末条刊例更新', '末条成本', '9', '2018-08-20 14:38:50', 0, '上传', '时尚美妆', NULL, NULL, NULL);
-INSERT INTO `cPublicNumber` VALUES (35, '小思成长日记', 'Psy-diary', 280000, '28000', '25000', '15000', '10000', '/', '/', '60', '2018-08-31 23:11:03', 0, '上传', '教育亲子', '不刷', NULL, NULL);
-INSERT INTO `cPublicNumber` VALUES (36, '暖暖妈爱分享', 'nnmafx', 1200000, '不接', '不接', '85000', '80000', '/', '/', '61', '2018-08-31 23:11:03', 0, '上传', '教育亲子', '不刷', NULL, NULL);
+INSERT INTO `cPublicNumber` VALUES (35, '小思成长日记', 'Psy-diary', 280000, '28000', '25000', '15000', '10000', '/', '/', '60', '2018-09-07 21:48:10', 2, '上传', '教育亲子', '不刷', NULL, NULL);
+INSERT INTO `cPublicNumber` VALUES (36, '暖暖妈爱分享啊', 'nnmafx', 1200000, '不接', '不接', '85000', '80000', '/', '/', '61', '2018-09-07 23:58:13', 0, '平台', '教育亲子', '不刷', NULL, NULL);
 INSERT INTO `cPublicNumber` VALUES (37, '胖少女晚托班', 'babyfatlady_1987', 400000, '85000', '80000', '/', '/', '/', '/', '62', '2018-08-31 23:11:03', 0, '上传', '教育亲子', '不刷', NULL, NULL);
 INSERT INTO `cPublicNumber` VALUES (38, '三个妈妈六个娃', 'pkumum', 200000, '35000', '30000', '/', '/', '/', '/', '63', '2018-08-31 23:11:03', 0, '上传', '教育亲子', '不刷', NULL, NULL);
 INSERT INTO `cPublicNumber` VALUES (39, '中小学阅读吧', 'jiazhangbest', 350000, '15000', '12000', '/', '/', '/', '/', '66', '2018-08-31 23:11:03', 0, '上传', '教育亲子', '半刷', NULL, NULL);
 INSERT INTO `cPublicNumber` VALUES (40, '花姐小食光', 'totyummy', 100000, '18000', '16,000', '/', '/', '/', '/', '64', '2018-08-31 23:11:03', 0, '上传', '教育亲子', '半刷', NULL, NULL);
 INSERT INTO `cPublicNumber` VALUES (41, '家长家', 'jiazhanghome', 830000, '38000', '35000', NULL, '13000', '/', '/', '65', '2018-08-31 23:11:03', 0, '上传', '教育亲子', '半刷', NULL, NULL);
-INSERT INTO `cPublicNumber` VALUES (42, '伊姐看电影', 'eemovie', 760000, '65000', '60000', '/', '32000', '/', '/', '68', '2018-08-31 23:27:32', 2, '上传', 'kol', '全刷', NULL, NULL);
+INSERT INTO `cPublicNumber` VALUES (42, '伊姐看电影', 'eemovie', 760000, '65000', '60000', '/', '32000', '/', '/', '68', '2018-09-17 22:38:58', 2, '平台', 'kol', '全刷', NULL, '/public/upload/1537195046137_psb (3).jpeg,/public/upload/1537195111700_16.jpeg');
 COMMIT;
 
 -- ----------------------------
@@ -167,14 +223,16 @@ CREATE TABLE `cUser` (
   `remark` varchar(45) DEFAULT NULL,
   `pwd` varchar(45) NOT NULL,
   `openId` varchar(45) NOT NULL,
+  `isDeptAdmin` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of cUser
 -- ----------------------------
 BEGIN;
-INSERT INTO `cUser` VALUES (18, '管理员', '13857476603', '研发部', '技术总监', '', 'E10ADC3949BA59ABBE56E057F20F883E', '4B9783D59B714AA277712B0F44B4091D');
+INSERT INTO `cUser` VALUES (18, '管理员', '13857476603', 'sale', '技术总监', '', 'E10ADC3949BA59ABBE56E057F20F883E', '4B9783D59B714AA277712B0F44B4091D', 1);
+INSERT INTO `cUser` VALUES (19, '测试财务权限', '18870420550', 'sale', '财务', '', 'E10ADC3949BA59ABBE56E057F20F883E', '949CEDE989C3445A39D7ADECB13C1CB6', 0);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
