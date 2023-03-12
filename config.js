42baca94249ffeb0eb22f6ec2f098defb686be58
wjs <wangjunsheng@gumingnc.com>
2023-03-12 19:18:47 +0800

    feat

--- config.js
+++ config.js
@@ -2,12 +2,11 @@ const CONF = {
     port: '5757',
     rootPathname: './',
     mysql: {
-        host: '47.97.182.189',
+        host: 'localhost',
         port: 3306,
-        user: 'root',
+        user: 'dms',
         db: 'libaisi_crm',
-        pass: 'qwer123456',
-        char: 'utf8mb4'
+        pass: 'PASSword123!',
     }
 }
 
